6c2f40dbaa4843f4b649437f3c6c7c5acd3d707a
Yulia Savine <149355830+ysavine@users.noreply.github.com>
2025-10-29 00:52:43 -0400

    skills

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,8 +6,8 @@ const experiences = [
     role: 'Software Engineer Intern',
     period: 'May 2025 - August 2025',
     description:
-      'Engineered and launched an internal full stack profiling tool for Google Drive.',
-    tags: ['Distributed Systems', 'Profiling', 'Java'],
+      'Engineered and launched a full stack profiling tool for Google Drive.',
+    tags: ['Distributed Systems', 'Request Profiling', 'Java', 'pprof'],
   },
   {
     company: 'Duke University Innovation Co-Lab',
@@ -15,7 +15,7 @@ const experiences = [
     period: 'August 2024 - Present',
     description:
       'Build full-stack applications and educational tools for 17,000+ Duke community members and Innovation Co-Lab clients.',
-    tags: ['Full Stack Development', 'Azure Cognitive Speech Services'],
+    tags: ['Full Stack Development', 'Azure Cognitive Speech Services', 'Python', 'FastAPI', 'Docker', 'Svelte', 'Javascript', 'React.js', 'HTML/CSS', 'MongoDB'],
   },
   {
     company: 'Duke University Computer Science Department',
@@ -31,7 +31,7 @@ const experiences = [
     period: 'May 2024 - July 2024',
     description:
       'Developed search and text generation features, streamlining the process for 8000+ researchers and faculty to find collaborators, discover funding opportunities, and generate a profile on Scholars at Duke.',
-    tags: ['Full Stack Development', 'Natural Language Processing', 'Large Language Models'],
+    tags: ['Full Stack Development', 'Natural Language Processing', 'ChromaDB', 'LangChain', 'HTML/CSS'],
   },
 ]
 
